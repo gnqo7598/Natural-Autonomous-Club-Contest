1f019947ab615be5a913110854b125917c2380cc
sjr0628 <sjr0628@naver.com>
2020-10-26 21:46:48 +0900

    코드 수정, 태그 추가
    플레이어와 오브젝트 간의 상호작용 코드 추가, 태그 Wall 추가, 프리팹 Wall에 태그 Wall 적용

---
+++ 자연자율동아리 대회/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BoardManager : MonoBehaviour
+{
+    public int length = 8;
+    public int height = 8;
+    public GameObject Player;
+    public GameObject Destination;
+    public GameObject Tile;
+
+    private Transform boardHolder;
+    private List<Vector3> gridPositions = new List<Vector3>();
+
+    void InitializeList()
+    {
+        gridPositions.Clear();
+
+        for(int x = 0; x < length; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                gridPositions.Add(new Vector3(x, y, 0f));
+                if (x == 0 && y == 0 || x == length - 1 && y == height - 1)
+                    gridPositions.Remove(new Vector3(x, y, 0f));
+            }
+        }
+    }
+
+    void BoardSetup()
+    {
+        boardHolder = new GameObject("Board").transform;
+
+        for(int x = 0; x < length; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                GameObject TileArray = Instantiate(Tile, new Vector3(x, y, 0f), Quaternion.identity);
+
+                TileArray.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+    public void SetupScene()
+    {
+        BoardSetup();
+        InitializeList();
+        Instantiate(Destination, new Vector3(length - 1, height - 1, 0f), Quaternion.identity);
+        Instantiate(Player, new Vector3(0, 0, 0f), Quaternion.identity);
+    }
+
+}
---
+++ 자연자율동아리 대회/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public BoardManager boardManager;
+
+    int xLim, yLim;
+    int point = 100;
+
+    Vector2 xMove = new Vector2(1, 0);
+    Vector2 yMove = new Vector2(0, 1);
+
+    Vector2 lastMove;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            Destroy(collision.gameObject);
+            point += 1;
+            Debug.Log(point);
+        }
+
+        else if (collision.gameObject.tag == "Trap")
+        {
+            Destroy(collision.gameObject);
+            point -= 1;
+            Debug.Log(point);
+        }
+
+        else if (collision.gameObject.tag == "Wall")
+        {
+            transform.Translate(-lastMove);
+            point += 1;
+            Debug.Log(point);
+        }
+    }
+
+    void MoveMyself()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (transform.position.x < xLim - 1)
+            {
+                lastMove = xMove;
+                transform.Translate(xMove);
+                point -= 1;
+                Debug.Log(point);
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (transform.position.y < yLim - 1)
+            {
+                lastMove = yMove;
+                transform.Translate(yMove);
+                point -= 1;
+                Debug.Log(point);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xLim = boardManager.length;
+        yLim = boardManager.height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
